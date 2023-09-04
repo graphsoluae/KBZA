6196e8d8ca3614f8168f9bcb64310b5bcc8cb9d1
graphsoluae <128033359+graphsoluae@users.noreply.github.com>
2023-09-04 18:19:58 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -34,3 +34,14 @@ t3BTN.addEventListener('click', function () {
     t2BTN.classList.remove('active');
     t3BTN.classList.add('active');
 });
+
+function clearCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+}
