4e9fd6f1ea0e439376767578260f80c8e027fb26
graphsoluae <128033359+graphsoluae@users.noreply.github.com>
2023-09-04 19:07:30 +0400

    Update contact1.js

--- script/contact1.js
+++ script/contact1.js
@@ -3,14 +3,14 @@ saveBtn.addEventListener("click", function () {
 
 
 
-    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:\nN: Sara;Ben; Ammar;\nTEL;TYPE=CELL:+971523253163\nEMAIL:sara.benammar@kbza.sch.ae\nORG:Khalifa Bin Zayed Al Awal School\nTITLE:Principal\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nPHOTO;TYPE=JPEG;ENCODING=b;VALUE=URL:file:///C:/Users/GS_Motion/Pictures/Graphic%20Solutions/Works/NFC%20Works/KBZA/images/profile/pricipal.png\nTITLE:\nREV:Mon Sep 04 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:\nN: Mansoori;Muzna; Al;\nTEL;TYPE=CELL:+971501775604\nEMAIL:muzna.almansoori@kbza.sch.ae\nORG:Khalifa Bin Zayed Al Awal School\nTITLE:Admissions Manager\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nTITLE:\nREV:Mon Sep 04 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
 
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
 
     const newLink = document.createElement('a');
-    newLink.download = "Sara Ben Ammar" + ".vcf";
-    newLink.textContent = "Sara Ben Ammar";
+    newLink.download = "Muzna Al Mansoori" + ".vcf";
+    newLink.textContent = "Muzna Al Mansoori";
     newLink.href = url;
 
 
@@ -19,13 +19,3 @@ saveBtn.addEventListener("click", function () {
 });
 
 
-function clearCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-    }
-}
