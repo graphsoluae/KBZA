f8b16332daf6ee1a63d8df66bd253c6b105ee75b
graphsoluae <128033359+graphsoluae@users.noreply.github.com>
2023-09-04 19:10:14 +0400

    Update contact15.js

---
+++ script/contact15.js
@@ -0,0 +1,21 @@
+var saveBtn = document.getElementById("save-contact");
+saveBtn.addEventListener("click", function () {
+
+
+
+    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:\nN: Aljefri;Hanan;\nTEL;TYPE=CELL:+971507913341\nEMAIL:hanan.aljefri@kbza.sch.ae\nORG:Khalifa Bin Zayed Al Awal School\nTITLE:Vice Principal\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nTITLE:\nREV:Mon Sep 04 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
+
+    var blob = new Blob([vcard], { type: "text/vcard" });
+    var url = URL.createObjectURL(blob);
+
+    const newLink = document.createElement('a');
+    newLink.download = "Hanan Aljefri" + ".vcf";
+    newLink.textContent = "Hanan Aljefri";
+    newLink.href = url;
+
+
+
+    newLink.click();
+});
+
+
