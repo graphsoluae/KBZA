044574361572bb322ad07b3fef3d06a5e45bc2d1
graphsoluae <128033359+graphsoluae@users.noreply.github.com>
2023-09-05 12:50:34 +0400

    Update contact11.js

--- script/contact11.js
+++ script/contact11.js
@@ -3,7 +3,7 @@ saveBtn.addEventListener("click", function () {
 
 
 
-    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:\nN: Sara;Ben; Ammar;\nTEL;TYPE=CELL:+971523253163\nEMAIL:sara.benammar@kbza.sch.ae\nORG:Khalifa Bin Zayed Al Awal School\nTITLE:Principal\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nTITLE:\nREV:Mon Sep 04 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
+    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:\nN: Ammar;Sara; Ben;\nTEL;TYPE=CELL:+971523253163\nEMAIL:sara.benammar@kbza.sch.ae\nORG:Khalifa Bin Zayed Al Awal School\nTITLE:Principal\nADR; TYPE = WORK: ;;; Abudhabi;United Arab Emirates;;\nTITLE:\nREV:Mon Sep 04 2023 12: 55: 50 GMT + 0400(Gulf Standard Time)\nEND:VCARD";
 
     var blob = new Blob([vcard], { type: "text/vcard" });
     var url = URL.createObjectURL(blob);
